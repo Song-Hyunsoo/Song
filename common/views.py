d3e8119762ae4a72c84f8258003a495378db253e
Song-Hyunsoo <hssong0111@gmail.com>
2024-10-28 17:47:42 +0900

    500

--- common/views.py
+++ common/views.py
@@ -22,4 +22,7 @@ def signup(request):
 
 def page_not_found(request, exception):
     return render(request, 'common/404.html', {})
+
+def page_not_found(request, exception):
+    return render(request, 'common/500.html', {})
 # Create your views here.
