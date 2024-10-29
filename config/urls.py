486863b1762138d22b518a1a396b728ac820c6b9
Song-Hyunsoo <hssong0111@gmail.com>
2024-10-29 14:36:09 +0900

    '500'

--- config/urls.py
+++ config/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-
+from django.urls import path, include
 from pybo.views import base_views
 
+
+
+from pybo import views
 urlpatterns = [
-    path('pybo/', include('pybo.urls')),
-    path('common/', include('common.urls')),
     path('admin/', admin.site.urls),
-    path('', base_views.index, name='index'),  # '/' 에 해당되는 path
+    path('pybo/',include('pybo.urls')),
+    path('common/', include('common.urls')),
+    path('', base_views.index, name='index'),
 ]
 
 handler404 = 'common.views.page_not_found'
