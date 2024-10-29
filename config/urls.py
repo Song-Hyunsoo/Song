b8d660f9c4cb87704b3db80c19d5811b2bc1b6a5
Song-Hyunsoo <hssong0111@gmail.com>
2024-10-29 14:29:06 +0900

    '500'

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pybo.views import base_views
-from django.conf.urls import handler404
-from common.views import page_not_found
+
 
 
 from pybo import views
